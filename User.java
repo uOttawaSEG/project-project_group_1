4c48a17875d62f1bc860d5da67b06a0803992754
Zachary Djerdjouri <zdjer050@uottawa.ca>
2025-10-08 10:56:48 -0400

    Added User abstract class

---
+++ User.java
@@ -0,0 +1,10 @@
+public abstract class User{
+
+	// TO DO: Change access modifiers? (protected?)
+	public String firstName;
+	public String lastName;
+	public String emailAddress;
+	public String password;
+
+	public int phoneNumber; // Expected format: 8191237896
+}
