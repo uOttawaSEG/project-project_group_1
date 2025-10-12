ad5a8cd318114e4492612607779656740628e8f4
DD1608 <dragos.florian.daiciulescu@gmail.com>
2025-10-11 20:43:48 -0400

    Authentication (registering entity) and log out function

---
+++ app/src/main/java/com/example/otams/MainActivity2.java
@@ -0,0 +1,30 @@
+package com.example.otams;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2); // Make sure this layout exists
+
+        Button buttonReturn = findViewById(R.id.logout);
+        buttonReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // optional, removes MainActivity2 from back stack
+            }
+        });
+
+    }
+
+
+
+
+}
