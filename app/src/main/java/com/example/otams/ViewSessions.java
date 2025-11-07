553261bbc92641d4a0d3605d1aec8eead18c1165
Darren <darrenqu0810@outlook.com>
2025-11-07 12:35:44 -0500

    Comments added in ViewSessions

--- app/src/main/java/com/example/otams/ViewSessions.java
+++ app/src/main/java/com/example/otams/ViewSessions.java
@@ -89,6 +89,8 @@ public class ViewSessions extends AppCompatActivity {
 
         Button btnAdd = itemView.findViewById(R.id.btnAddSession);
         btnAdd.setOnClickListener(v -> {
+            // TODO
+            // Add an if else statement here
             slot.requestStatus = "PENDING";
             slot.studentEmail = studentEmail;
             tutorAvailabilityDao.update(slot);
